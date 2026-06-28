2a1f810926725414e7b0050e9cd3ec37fc8aa163
Rishabh Tivari <rishabhtivari428@gmail.com>
2026-06-28 22:17:41 +0530

    Applicant Part Finished

--- Frontend/src/features/auth/services/jobs.api.js
+++ Frontend/src/features/auth/services/jobs.api.js
@@ -1,24 +1,23 @@
-import axiosInstance  from "../api/axiosInstance";
+import axiosInstance from "../api/axiosInstance";
 
-export async function postJob(title, description, location, requirements, company, Salary, role){
-    const response = await axiosInstance.post(`/api/jobs/postjob`,{
-        title, 
-        description, 
-        location, 
-        requirements, 
+export async function postJob(title, description, location, requirements, company, Salary) {
+    const response = await axiosInstance.post(`/api/jobs/postjob`, {
+        title,
+        description,
+        location,
+        requirements,
         company,
         Salary,
-        role
     })
     return response.data
 }
 
-export async function getJob(){
+export async function getJob() {
     const response = await axiosInstance.get('/api/jobs/getjob')
     return response.data
 }
 
-export async function deleteJob(id){
+export async function deleteJob(id) {
     const response = await axiosInstance.delete(`/api/jobs/deletejob/${id}`)
     return response.data
 }
